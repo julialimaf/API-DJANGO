88401a1e42fd2e257f8a5e0e9534f28132150aeb
Júlia Ferreira <anajulia.ferreira@catskillet.com>
2025-08-25 17:00:36 -0300

    Integrado com o Postegres

--- api_root/settings.py
+++ api_root/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api_rest',
     'corsheaders',
+    'psycopg2',
 
 ]
 
@@ -77,12 +78,19 @@ WSGI_APPLICATION = 'api_root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Cadastros',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
